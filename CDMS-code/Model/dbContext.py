73d660f8e72a359aedecd9e8991465e801af3c3f
Enny <46939885+EnnyGit@users.noreply.github.com>
2021-06-25 13:14:38 +0200

    dbContext file added

--- CDMS-code/Model/dbContext.py
+++ CDMS-code/Model/dbContext.py
@@ -1,3 +1,36 @@
 import sqlite3
 
-con = sqlite3.connect('example.db')
+# Creates connection
+connection = sqlite3.connect('example.db')
+
+# Creates a Cursor object
+cursor = connection.cursor()
+
+# Execute SQL command
+cursor.execute('''CREATE TABLE stocks (
+    date text,
+    trans text,
+    symbol text,
+    qty real,
+    price real
+)''')
+
+# Insert a row of data
+cursor.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+
+# Save (commit) the changes
+connection.commit()
+
+# Close connection
+connection.close()
+
+# This is the qmark style:
+cursor.execute("create table lang (name, first_appeared)")
+cursor.execute("INSERT INTO lang VALUES (?, ?)", ("C", 1972))
+
+# Datatypes
+# NULL
+# INTEGER
+# REAL 
+# TEXT
+# BLOB
