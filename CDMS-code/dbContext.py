cd45b48c5a3600efa528336456ade8a980d2e6a4
Enny <46939885+EnnyGit@users.noreply.github.com>
2021-06-25 14:39:29 +0200

    Changed dbContext

--- CDMS-code/dbContext.py
+++ CDMS-code/dbContext.py
@@ -1,12 +1,32 @@
 import sqlite3
 import initialDB
 
-connection = sqlite3.connect('CDMS-data.db')
-cursor = connection.cursor()
+class SQLite:
+    Message = ""
 
-# This is the qmark style:
-#cursor.execute("create table lang (name, first_appeared)")
-#cursor.execute("INSERT INTO lang VALUES (?, ?)", ("C", 1972))
+    @staticmethod
+    def Connect():
+        try:
+            connection = sqlite3.connect('CDMS-data.db')
+            SQLite.Message = "Connected"
+            return connection
+        except Exception as e:
+            SQLite.Message = e
 
-connection.commit()
-connection.close()
+    @staticmethod
+    def Close(self, Connection):
+        Connection.close()
+
+    #cursor = connection.cursor()
+    #cursor.execute("INSERT INTO user VALUES ('Admin', 'Password123', 'Enny', 'Adeoye', '2021')")
+
+
+    # This is the qmark style:
+    #cursor.execute("create table lang (name, first_appeared)")
+    #cursor.execute("INSERT INTO lang VALUES (?, ?)", ("C", 1972))
+
+    #connection.commit()
+    #connection.close()
+
+SQLite.Connect()
+print(SQLite.Message)
